15bfceb638efe3e15f2aa8512162a01611de1c09
rezekiz <149320958+rezekiz@users.noreply.github.com>
2023-11-22 19:51:15 +0000

    Adicionado notebook sobre alinhamento

--- auxiliares.py
+++ auxiliares.py
@@ -48,179 +48,136 @@ def aprimorar_dna(seq):
     """
     return seq.upper().replace(" ","")
      
-def contar_bases(seq):
 
-    """
-    Devolve um dicionário com as frequências das bases de uma string de ADN (A, C, T e G)
+''' Função usada para formatar o conteúdo, que irá ajudar na formatação da matriz '''
 
-    
-    Parameters
-    -------------
-    seq : str
-        Uma string que representa a sequência de ADN
+def formatar(conteudo):
 
-    
-    Returns
-    -------------
-    resultado : dict
-        Dicionário com as frequências das bases da sequência introduzida
-        As chaves são as bases "A", "T", "C" e "G" e os valores as frequências correspondentes
-
-        
-    Raises
-    ----------
-    ValueError
-        No caso da string inserida não ser válida
-    
-    """
-
-    if validar_dna(seq) is False:
-        raise ValueError ("A sequência inserida é inválida")
-
-    seq = aprimorar_dna(seq)
-    
-    contA = seq.count("A")
-    contT = seq.count("T")
-    contC = seq.count("C")
-    contG = seq.count("G")
-    
-    resultado = {"A" : contA, "T" : contT, "C" : contC, "G" : contG}
-    
-    return resultado
-
-def conteudo_gc(seq):
-    
-    """   
-    Devolve a percentagem do conteúdo GC presente numa sequência de ADN
-
-    
-    Parameters
-    -------------
-    seq : str
-        Uma string que representa a sequência de ADN
-
-    
-    Returns
-    -------------
-    resultado : float
-        Percentagem do conteúdo GC na sequência de ADN
-
-        
-    Raises
-    ----------
-    ValueError
-        No caso da string inserida não ser válida
-    
-    """
-
-    if validar_dna(seq) is False:
-        raise ValueError ("A sequência inserida é inválida")
-    
-    seq = aprimorar_dna(seq)
-    
-    conteudo_g = seq.count("G")
-    conteudo_c = seq.count("C")
-    conteudo_gc = round((conteudo_g + conteudo_c) / len(seq) * 100, 1)
-    
-        
-    return conteudo_gc
-
-conteudo_gc("gcta") 
-
-def transc(seq):
-
-    '''
-        Função que itera sobre uma sequência de DNA e devolve
-    a sequência transcrita.
-
-    Lógica:
-    A -> U
-    T -> A
-    C -> G
-    G -> C
-
-    Parameters:
-    ------------
-        seq : str Sequência de DNA
-            Uma string que representa a sequência de DNA
-    '''
-
-    if not validar_dna(seq):
-        print("Sequência inválida")
+    if not isinstance(conteudo, (int,float,str)): 
+        print('Inválido, tem de ser um número ou string.')
         return None
 
-    '''
-    Aqui é utilizada a função 'validar_dna' para garantir que a sequência não possui caracteres inválidos
-    '''
+    if type(conteudo) is int:
 
-    dna = aprimorar_dna(seq)
-    mrna = ''
+        '''Se x for um int converte-o em str e alinha-o à direita (por causa do indicador de formatação '>')
+  
+        Se pretendessemos alinhar à esquerda o indicador de formatação seria '<'
+    
+        A parte 3d significa o número mínimo de dígitos de largura que queremos no alinhamento.''' 
+    
 
-    '''
-    A variável dna vai buscar a sequência aprimorada pela função definida anteriormente
-    '''
+        return f"{conteudo:>3d}"
 
-    for base in dna:
-        if base == 'A':
-          mrna += 'U'
-        elif base == 'T':
-          mrna += 'A'
-        elif base == 'C':
-          mrna += 'G'
-        elif base == 'G':
-          mrna += 'C'
-    '''
-    Returns:
-    --------
-        str
-          Sequência de mrna
-    '''
-    return mrna
+    else:
 
+        '''
+        Caso não seja um int alinha na mesma à direita mas aqui o fator d já não se aplica uma vez que o format
+        code d não se aplica a strs por exemplo.
+        '''
 
-def complemento_inverso(sequencia : str) -> str:
-    '''
-    Função que itera sobre uma sequência de DNA e devolve
-    o complemento reverso de cada nucleotido.
+        return f"{conteudo:>3}"
+    
+''' Função usada para imprimir a matriz corretamente formatada '''
 
-    Lógica:
-    A -> T
-    T -> A
-    C -> G
-    G -> C
+def print_matrix(primeira_string : str, segunda_string : str, matriz_scores : list):
+  '''
+  S1 -> primeira string
+  S2 -> segunda string
+  M  -> matriz
 
-    Parameters
-    ----------
+  '''
 
-    sequencia : str
-        sequência de DNA válida
+  '''
+  Começamos por construir e formatadar a nossa linha de colunas.
 
-    Returns
-    -------
-    str
-        sequência de nucléotidos complementar à cadeia fornecida
+  Para isso convertemos a string 1, correspondente às colunas, em lista e iteramos elemento a elemento.
 
-    '''
-    assert validar_dna(sequencia)
+  Usamos operador * para fazer unpack da lista, carater a carater. Desta forma não imprime a lista como um todo. 
+  Neste caso usamos definimos o parâmetro sep da função print, indicando 2 espaços em branco para espaçar melhor a nossa linha de colunas.
+  
+  '''
+  
+  print(*list(" " + primeira_string), sep = '   ')
 
-    complementar = '' # inicializamos a cadeia complementar
+  '''
+  Formatamos o conteúdo da nossa coluna de linhas e respetivo conteúdo das linhas.
 
-    for base in sequencia.upper():
-        if base == 'A':
-            complementar += 'T'
-        elif base == 'T':
-            complementar += 'A'
-        elif base == 'C':
-            complementar += 'G'
-        elif base == 'G':
-            complementar += 'C'
+  Para isto usamos um ciclo for que irá iterar por cada elemento do emparelhamento da nossa segunda string e a matriz de scores obtida.
 
-    return complementar
+  '''
 
+  for linha, conteudo_linha in zip(segunda_string, matriz_scores):
+    #print('linha', linha)
+    conteudo_formatado = [formatar(conteudo) for conteudo in conteudo_linha]
+    print(linha, *conteudo_formatado)
 
+  print()       # Adicionamos um alinha em branco para separar do conteúdo seguinte.
+
+'''
+Função auxiliar que reconstroi as sequências devidamente alinhadas.
 
 '''
 
-SECÇÃO PARA FUNÇÃO GETORFS ASSIM QUE ESTIVER DEVIDAMENTE DOCUMENTADA
+def reconstroi(string_1, string_2, matriz_traceback):
+  '''
+  Parameteres
+  -----------
 
-'''
+  string_1 : str
+    primeira string para alinhamento
+
+  string_1 : str
+    segunda string para alinhamento
+
+  matriz_traceback : list
+    lista de listas correspondente a uma matriz com direções para percorrer as strings
+    e reconstruir os alinhamentos
+
+  Returns
+  -------
+
+  tuple
+    devolve um tuple com as matrizes alinhadas
+  
+  '''
+  
+  # Iniciamos as variáveis-resultado
+  string_1_alinhada = ''
+  string_2_alinhada = ''
+
+  # Criamos os contador para iterar, que nos devolverá o último elemento de cada string
+  colunas = len(string_1) - 1
+  linhas  = len(string_2) - 1
+  
+
+  # Itera pela matriz traceback e em função da direção calculada determina a posição e o elemento a atribuir.
+
+  while matriz_traceback[linhas][colunas] != 0:
+    print(linhas, colunas, string_2[linhas], string_1[colunas], matriz_traceback[linhas][colunas])
+
+    # Se na matriz andarmos na diagonal significa que os elementos são iguais, então saltam para o alinhamento
+    if matriz_traceback[linhas][colunas] == 'D':
+      string_1_alinhada = string_1[colunas] + string_1_alinhada  # De notar que adicionamos sempre a string alinhada no fim 
+      string_2_alinhada = string_2[linhas]  + string_2_alinhada  # pois estamos a construí-la de trás para a frente
+
+      linhas  -= 1                                               # Subtraímos 1 a linhas e colunas para avançar para percorrer
+      colunas -= 1                                               # mais um nível na nossa matriz traceback
+
+    # Se na matriz andarmos para cima, significa que não são iguais e conservamos o elemento da segunda string
+    elif matriz_traceback[linhas][colunas] == 'C':
+      string_1_alinhada = '-'              + string_1_alinhada
+      string_2_alinhada = string_2[linhas] + string_2_alinhada
+
+      linhas  -= 1                                              # Como andamos para cima só avançamos nas linhas
+
+    # Se na matriz andarmos para a esquerda, significa que não são iguais e conservamos o elemento da primeira string
+    elif matriz_traceback[linhas][colunas] == 'E':
+      string_1_alinhada = string_1[colunas] + string_1_alinhada
+      string_2_alinhada = '-'               + string_2_alinhada
+
+      colunas -= 1                                               # Como andamos para a esquerda só avançamos nas colunas
+  print()
+  print(string_1_alinhada, string_2_alinhada, sep = "\n")        # Imprimimos as nossas strings alinhadas em linhas separadas
+
+  return (string_1_alinhada, string_2_alinhada)                  # Obtemos as strings alinhadas reconstruídas para uso futuro
+
